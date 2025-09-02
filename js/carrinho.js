828f6c51c768a4c06774010329a7dd4c56d0b470
Yan-Gabriel <yangabriel2.0.yg@gmail.com>
2025-09-02 00:43:25 -0300

    adiciona funcionalidade de salvar produto no carrinho

---
+++ js/carrinho.js
@@ -0,0 +1,60 @@
+/*
+Objetivo 1 - quando clicar no botão de adicionar ao carrinho:
+    - atualizar o contador
+    - adicionar o produto no localStorage
+    - atualizar a tabela HTML do carrinho
+
+Objetivo 2 - remover produtos do carrinho:
+    - ouvir o botão de deletar
+    - remover do localStorage
+    - atualizar o DOM e o total
+
+Objetivo 3 - atualizar valores do carrinho:
+    - ouvir mudanças de quantidade
+    - recalcular total individual
+    - recalcular total geral
+*/
+
+const botoesAdicionarAoCarrinho = document.querySelectorAll('.adicionar-ao-carrinho');
+
+
+botoesAdicionarAoCarrinho.forEach(botao => {
+    botao.addEventListener('click', (evento) => {
+        const elementoProduto = evento.target.closest(".produto");
+        const produtoId = elementoProduto.dataset.id;
+        const produtoNome = elementoProduto.querySelector('.nome').textContent;
+        const produtoImagem = elementoProduto.querySelector('img').getAttribute('src');
+        const produtoPreco = parseFloat(elementoProduto.querySelector('.preco').textContent.replace('R$ ', '').replace('.','').replace(',', '.'));
+
+        //BUSCAR A LISTA DE PRODUTOS NO LOCALSTORAGE
+        const carrinho = obterProdutosDoCarrinho(); // array
+
+        //VERIFICAR SE O PRODUTO JÁ EXISTE NO CARRINHO
+        const existeProduto = carrinho.find(produto => produto.id === produtoId); // se encontrar, retorna o produto, se não, retorna undefined
+        if (existeProduto) {
+            existeProduto.quantidade += 1; // se o produto já existe, incrementa a quantidade
+        } else {
+            //ADICIONAR O PRODUTO AO CARRINHO
+            const produto = {
+                id: produtoId,
+                nome: produtoNome,
+                imagem: produtoImagem,
+                preco: produtoPreco,
+                quantidade: 1
+            };
+            carrinho.push(produto);
+        }
+        salvarProdutoNoCarrinho(carrinho); // salva o array atualizado no localStorage
+    });
+})
+
+// Função para salvar os produtos do carrinho no localStorage
+function salvarProdutoNoCarrinho(carrinho) {
+    localStorage.setItem('carrinho', JSON.stringify(carrinho));
+}
+
+// Função para obter os produtos do carrinho armazenados no localStorage
+function obterProdutosDoCarrinho() {
+    const produtos = localStorage.getItem('carrinho'); // 'carrinho' é a chave onde os produtos estão armazenados
+    return produtos ? JSON.parse(produtos) : []; // se produtos for nulo, retorna um array vazio
+}
